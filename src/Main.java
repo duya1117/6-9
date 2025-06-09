718d229cbdbe9f69cc54ca3d97e51999d3fa2db5
USER <rhs9827@naver.com>
2025-06-09 10:38:51 +0900

    Method-Overloading2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
-    public static int add(int a, int b) {
-        return a + b;
+    public static void greet(){
+        System.out.println("Hello, World!");
     }
-
-    public static int add(int a, int b, int c){
-        return a + b + c;
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+    public static void greet(String name, int age){
+        System.out.println("Hello, " + name + "!" + " age " + age);
     }
     public static void main(String[] args) {
-        System.out.println("a + b = " + add(1,2));
-        System.out.println("a + b + c = " + add(3,4,5));
+        greet();
+        greet("Alice");
+        greet("Alice",18);
     }
 }
