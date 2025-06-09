2501418da9b527c092668fcd5f5ff8592ad540fe
USER <rhs9827@naver.com>
2025-06-09 13:37:56 +0900

    Arrays2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-//        int[] numbers = {1,2,3,4,5,6,7};
-//
-//        for(int i = 6; i >= 0; i--){
-//            System.out.println(numbers[i]);
-//        }
-
         int[] numbers = new int [5];
-        int num;
-        int j;
-
         for(int i = 0; i < numbers.length; i++){
-            numbers[i] = i + 1;
-        }
-        for(int i = 0; i < numbers.length; i++){
-            num = numbers[i];
-            j = (int)(Math.random() * (numbers.length -1) +1);
-                numbers[i] = numbers[j];
-                numbers[j] = num;
+            numbers[i] = (i+1)*2;
             System.out.println(numbers[i]);
         }
+
+        int sum = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        System.out.println(sum);
     }
 }
