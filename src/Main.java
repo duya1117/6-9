88326aa59aeab36530f93f967a2821acf4d875a3
USER <rhs9827@naver.com>
2025-06-09 10:08:54 +0900

    Method-Overloading

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
+    public static int add(int a, int b, int c){
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        System.out.println("a + b = " + add(1,2));
+        System.out.println("a + b + c = " + add(3,4,5));
     }
 }
